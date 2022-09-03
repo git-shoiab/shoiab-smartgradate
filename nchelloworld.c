3b45878867530e39846dd5cd322e677e3c9d99b4
git-shoiab <98313679+git-shoiab@users.noreply.github.com>
2022-09-03 12:16:03 +0530

    Add files via upload

---
+++ nchelloworld.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello and Welcome to Golden Touch Smart Graduates program of New College....");
+}
---
+++ ncioprog.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    printf("Please enter a number...");
+
+    int num1;
+
+    scanf("%d",&num1);
+
+    printf("The number entered is...:%d",num1);
+
+    float pi=3.14;
+
+    printf("Please enter a different unga pi value...");
+
+    scanf("%f",&pi);
+
+    printf("unga pila...:%f",pi);
+
+    printf("Please press Y to continue or N to discontinue");
+
+    char c='y';
+
+    scanf("%c",&c);
+
+    printf("The character entered is..:%c",c);
+}
