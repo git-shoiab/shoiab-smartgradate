34801ab786c3d84606a4c93045ad7d8e1724a7f5
git-shoiab <98313679+git-shoiab@users.noreply.github.com>
2022-09-30 10:54:31 +0530

    Add files via upload

---
+++ aa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+
+	if(i!=5){//if you give , then the entire thing becomes expressions - left side expression return non zero and right side also returns non zero
+        printf("true");
+	}
+	else{
+        printf("false");
+	}
+
+	if(printf("hello"));
+}
